528ee0d7ffeea0070bdd4c6d7a93882e7922dd9a
Marciano Pasa <pasamarciano@edu.unisinos.br>
2023-10-08 13:33:17 -0300

    Câmera na aba 3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  imageSource: any;
 
+  constructor(private domSanitizer : DomSanitizer) {}
+
+  takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Prompt,
+      saveToGallery: true
+    });
+
+    this.imageSource = this.domSanitizer.bypassSecurityTrustUrl(image.webPath ? image.webPath : "");
+  }
+
+  getPhoto() {
+    return this.imageSource;
+  }
 }
