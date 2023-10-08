87dd58fc54cc663365f5453bae1105b74f4265e6
henriquegranatto <henrique@gohusky.net>
2023-10-08 13:14:59 -0300

    Estrutura da lista de locais

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  filtroRestaurantes: boolean = false;
+  filtroCinemas: boolean = false;
+  filtroParques: boolean = false;
+  
   constructor() {}
 
+  locais = [
+    {
+      nome: 'Restaurante A',
+      tipo: 'Restaurante',
+      endereco: 'Endereço do Restaurante A',
+      linkGoogleMaps: 'https://maps.google.com/?q=Restaurante+A',
+    },
+    {
+      nome: 'Cinema B',
+      tipo: 'Cinema',
+      endereco: 'Endereço do Cinema B',
+      linkGoogleMaps: 'https://maps.google.com/?q=Cinema+B',
+    },
+    {
+      nome: 'Parque C',
+      tipo: 'Parque',
+      endereco: 'Endereço do Parque C',
+      linkGoogleMaps: 'https://maps.google.com/?q=Parque+C',
+    },
+    // Adicione mais locais aqui
+  ];
+
+  get locaisFiltrados() {
+    return this.locais.filter((local) => {
+      return (
+        (!this.filtroRestaurantes || local.tipo === 'Restaurante') &&
+        (!this.filtroCinemas || local.tipo === 'Cinema') &&
+        (!this.filtroParques || local.tipo === 'Parque')
+      );
+    });
+  }
 }
